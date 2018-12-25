83b3bf59189403bbb68080a576f7284659ab9b13
Boyuan Zhang 2 <Boyuan Zhang 2@PC-of-Boyuan-Zhang>
2018-12-25 13:11:09 +0100

    Map

---
+++ JSE/Map/Map/src/com/boyuanzhang/hashmap/FindMaxMap.java
@@ -0,0 +1,17 @@
+package com.boyuanzhang.hashmap;
+
+import java.util.Map;
+
+public class FindMaxMap {
+	
+	public static int findMaxIntegerKey(Map<Integer,Integer> map) {
+		Map.Entry<Integer, Integer> maxEntry = null;
+
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+				maxEntry = entry;
+			}
+		}
+		return maxEntry.getKey();
+	}
+}
