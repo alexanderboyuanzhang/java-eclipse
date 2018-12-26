2d1aeb511537b02bd0612888c51ae680ab0ed692
alexanderboyuanzhang <46047097+alexanderboyuanzhang@users.noreply.github.com>
2018-12-26 12:27:38 +0100

    Loop Print List

---
+++ JSE/list/List/src/com/list/LoopPrintList.java
@@ -0,0 +1,13 @@
+package com.list;
+
+import java.util.List;
+
+public class LoopPrintList {
+	
+	static void loopPrintList(List<?> list) {
+		for(Object l: list) {
+			System.out.println(l.toString());
+		}
+	}
+
+}
