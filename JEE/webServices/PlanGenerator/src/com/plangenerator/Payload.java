00be06904f828329947520b6f9756fbb71a090f3
alexanderboyuanzhang <46047097+alexanderboyuanzhang@users.noreply.github.com>
2019-02-04 09:32:08 +0100

    Plan Generator

---
+++ JEE/webServices/PlanGenerator/src/com/plangenerator/Payload.java
@@ -0,0 +1,48 @@
+package com.plangenerator;
+
+public class Payload {
+
+	private double loanAmount;
+	private double nominalRate;
+	private int duration;
+	private String startDate;
+
+	public double getLoanAmount() {
+		return loanAmount;
+	}
+
+	public void setLoanAmount(double remainingOutstandingPrincipal) {
+		this.loanAmount = remainingOutstandingPrincipal;
+	}
+
+	public double getNominalRate() {
+		return nominalRate;
+	}
+
+	protected void setNominalRate(double nominalRate) {
+		this.nominalRate = nominalRate;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	@Override
+	public String toString() {
+		return "{ \"loanAmount\": \"" + loanAmount + "\", \"nominalRate\": \"" + nominalRate + "\", \"duration\": \""
+				+ duration + "\", \"startDate\": \"" + startDate + "\" }";
+	}
+
+}
---
+++ JEE/webServices/PlanGenerator/src/com/plangenerator/services/LoansService.java
@@ -0,0 +1,30 @@
+package com.plangenerator.services;
+
+import java.text.ParseException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.plangenerator.Payload;
+import com.plangenerator.businesslogics.Calculations;
+
+@Path("/")
+public class LoansService {
+	
+	@POST
+	@Path("/generate-plan")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Payload payload) throws ParseException {
+		System.out.println("service...");
+		Calculations calculations = new Calculations(payload);
+		calculations.calculatePlan();
+		//"Payload saved" 
+		String result = calculations.toString();
+		return Response.status(201).entity(result).build();
+
+	}
+
+}
