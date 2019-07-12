ceed1201c9108d7a51089ffc2c2a34690a8f176c
alexanderboyuanzhang <alexander.boyuan.zhang@gmail.com>
2019-07-12 18:04:48 +0200

    SHA-256

--- Hackerrank/src/com/hackerrank/boyuanzhang/StdinStdout.java
+++ Hackerrank/src/com/hackerrank/boyuanzhang/StdinStdout.java
@@ -7,9 +7,7 @@ public class StdinStdout {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		while (scan.hasNextInt()) {
-
 			int a = scan.nextInt();
-
 			System.out.println(a);
 		}
 	}
