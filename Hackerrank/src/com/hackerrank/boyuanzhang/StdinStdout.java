35609c5ace516c53bd5664e61733ad9d1fb0d94d
alexanderboyuanzhang <alexander.boyuan.zhang@gmail.com>
2019-07-12 17:34:02 +0200

    StdinStdout

---
+++ Hackerrank/src/com/hackerrank/boyuanzhang/StdinStdout.java
@@ -0,0 +1,16 @@
+package com.hackerrank.boyuanzhang;
+
+import java.util.Scanner;
+
+public class StdinStdout {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNextInt()) {
+
+			int a = scan.nextInt();
+
+			System.out.println(a);
+		}
+	}
+}
