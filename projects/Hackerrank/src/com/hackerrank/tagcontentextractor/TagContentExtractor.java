83680f528360a73b75b70bbba9ac97b1a8f96bde
alexanderboyuanzhang <alexander.boyuan.zhang@gmail.com>
2019-07-22 15:29:26 +0200

    TagContentExtractor

---
+++ projects/Hackerrank/src/com/hackerrank/tagcontentextractor/TagContentExtractor.java
@@ -0,0 +1,16 @@
+package com.hackerrank.tagcontentextractor;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TagContentExtractor {
+
+	// String testString = "<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while<par>";
+
+	public static String processLine(String str) {
+
+		return "";
+	}
+
+}
